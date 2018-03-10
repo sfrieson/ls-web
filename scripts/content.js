68fa8fa535aeb7bb4eebf7e864a48facc081f9ec
Steven Frieson <sfrieson@gmail.com>
2018-03-10 16:57:21 -0500

    Lints

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,28 @@
-function render(arr){
-    console.log(arr);
-    var main = $('main');
-    for (var i = 0; i < arr.length; i++) {
-        var div = $('<div>');
-        div.append($('<h4>').html(arr[i].title.rendered));
-        div.append($('<p>').html(arr[i].content.rendered));
-        $('#loading').remove();
-        main.append(div);
+/* globals $ */
+function render (arr) {
+  console.log(arr);
+  var main = $('main');
+  for (var i = 0; i < arr.length; i++) {
+    var div = $('<div>');
+    div.append($('<h4>').html(arr[i].title.rendered));
+    div.append($('<p>').html(arr[i].content.rendered));
+    $('#loading').remove();
+    main.append(div);
+  }
+}
+function getArticles (cat, callback) {
+  $.get({
+    url: 'http://lesliesatin.com/wp/wp-json/wp/v2/posts?filter[category_name]=' + cat,
+    success: function (response) {
+      $(function () { callback(response); });
     }
+  });
 }
-function getArticles(cat, callback){
-    $.get({
-        url: "http://lesliesatin.com/wp/wp-json/wp/v2/posts?filter[category_name]=" + cat,
-        success: function(response){
-            $(function(){callback(response);});
-        }
-    });
-}
-var category = window.location.pathname.substr(1, window.location.pathname.indexOf('.')-1);
+var category = window.location.pathname.substr(1, window.location.pathname.indexOf('.') - 1);
 
 getArticles(category, render);
-$(function(){
-    if(window.innerWidth <= 966) {
-        $('.images').remove().insertBefore('#copyright');
-    }
+$(function () {
+  if (window.innerWidth <= 966) {
+    $('.images').remove().insertBefore('#copyright');
+  }
 });
--- scripts/writing.js
+++ scripts/writing.js
@@ -1,31 +1,32 @@
-function makeArticle(title, content) {
-    var div = $('<div class="article">');
-    div.append($('<h3>').html(title));
-    div.append($('<p class="cite">').html(content));
-    return div;
+/* globals $ */
+function makeArticle (title, content) {
+  var div = $('<div class="article">');
+  div.append($('<h3>').html(title));
+  div.append($('<p class="cite">').html(content));
+  return div;
 }
-function render(arr){
-    var main = $('main');
-    for (var i = 0; i < arr.length; i++) {
-        if($('#loading').length) $('#loading').remove();
-        main.append(
-            makeArticle(arr[i].title.rendered, arr[i].content.rendered)
-        );
+function render (arr) {
+  var main = $('main');
+  for (var i = 0; i < arr.length; i++) {
+    if ($('#loading').length) $('#loading').remove();
+    main.append(
+      makeArticle(arr[i].title.rendered, arr[i].content.rendered)
+    );
+  }
+}
+function getArticles (cat, callback) {
+  $.get({
+    url: 'http://lesliesatin.com/wp/wp-json/wp/v2/posts?filter[category_name]=' + cat,
+    success: function (response) {
+      $(function () { callback(response); });
     }
+  });
 }
-function getArticles(cat, callback){
-    $.get({
-        url: "http://lesliesatin.com/wp/wp-json/wp/v2/posts?filter[category_name]=" + cat,
-        success: function(response){
-            $(function(){callback(response);});
-        }
-    });
-}
-var category = window.location.pathname.substr(1, window.location.pathname.indexOf('.')-1);
+var category = window.location.pathname.substr(1, window.location.pathname.indexOf('.') - 1);
 
 getArticles(category, render);
-$(function(){
-    if(window.innerWidth <= 966) {
-        $('.images').remove().insertBefore('#copyright');
-    }
+$(function () {
+  if (window.innerWidth <= 966) {
+    $('.images').remove().insertBefore('#copyright');
+  }
 });
