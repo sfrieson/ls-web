70a7894f6aa6b4db17c944664ccd81cad306a1ec
Steven Frieson <sfrieson@gmail.com>
2018-03-10 18:05:24 -0500

    Removes jQuery as templater

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 /* globals $ */
 var category = window.location.pathname.substr(1, window.location.pathname.indexOf('.') - 1);
 
+function createElement (name, attributes, children) {
+  var el = document.createElement(name);
+  if (attributes) for (var attr in attributes) el.setAttribute(attr, attributes[attr]);
+  if (children) el.innerHTML = children;
+
+  return el;
+}
+
 function getArticles (cat, callback) {
   var endpoint = cat === 'about' ? 'pages' : 'posts';
   var query = '';
@@ -29,15 +37,20 @@ function getRenderFn (category) {
 
 // about.js
 function renderAbout (arr) {
-  console.log(arr);
-  var main = $('main');
-  for (var i = 0; i < arr.length; i++) {
-    var div = $('<div>');
-    div.append($('<h4>').html(arr[i].title.rendered));
-    div.append($('<p>').html(arr[i].content.rendered));
-    $('#loading').remove();
-    main.append(div);
-  }
+  var main = document.getElementsByTagName('main')[0];
+  var loading = document.getElementById('loading');
+  loading.parentElement.removeChild(loading);
+
+  arr.map(function (data) {
+    var div = createElement('div');
+    div.appendChild(
+      createElement('h4', null, data.title.rendered)
+    );
+    div.appendChild(
+      createElement('p', null, data.content.rendered)
+    );
+    return div;
+  }).forEach(main.appendChild.bind(main));
 }
 
 // choreography.js
@@ -50,44 +63,61 @@ var vimeo = function (url) {
     'frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 };
 function renderChoreography (arr) {
-  var main = $('main');
-  for (var i = 0; i < arr.length; i++) {
-    var div = $('<div>');
-    div.append($('<h4>').html(arr[i].title.rendered));
-    div.append($('<div class="embed-container">').html(vimeo(arr[i].content.rendered)));
-    $('#loading').remove();
-    main.append(div);
-  }
+  var main = document.getElementsByTagName('main')[0];
+  var loading = document.getElementById('loading');
+  loading.parentElement.removeChild(loading);
+
+  arr.map(function (data) {
+    var div = createElement('div');
+    div.appendChild(
+      createElement('h4', null, data.title.rendered)
+    );
+    div.appendChild(
+      createElement('div', {'class': 'embed-container'}, vimeo(data.content.rendered))
+    );
+
+    return div;
+  }).forEach(main.appendChild.bind(main));
 }
 
 // content.js
 function renderContent (arr) {
-  console.log(arr);
-  var main = $('main');
-  for (var i = 0; i < arr.length; i++) {
-    var div = $('<div>');
-    div.append($('<h4>').html(arr[i].title.rendered));
-    div.append($('<p>').html(arr[i].content.rendered));
-    $('#loading').remove();
-    main.append(div);
-  }
+  var main = document.getElementsByTagName('main')[0];
+  var loading = document.getElementById('loading');
+  loading.parentElement.removeChild(loading);
+
+  arr.map(function (data) {
+    var div = createElement('div');
+    div.appendChild(
+      createElement('h4', null, data.title.rendered)
+    );
+    div.appendChild(
+      createElement('p', null, data.content.rendered)
+    );
+
+    return div;
+  }).forEach(main.appendChild.bind(main));
 }
 
 // writing.js
 function makeArticle (title, content) {
-  var div = $('<div class="article">');
-  div.append($('<h3>').html(title));
-  div.append($('<p class="cite">').html(content));
+  var div = createElement('div', {'class': 'article'});
+  div.appendChild(
+    createElement('h3', null, title)
+  );
+  div.appendChild(
+    createElement('p', {'class': 'cite'}, content)
+  );
   return div;
 }
 function renderWriting (arr) {
-  var main = $('main');
-  for (var i = 0; i < arr.length; i++) {
-    if ($('#loading').length) $('#loading').remove();
-    main.append(
-      makeArticle(arr[i].title.rendered, arr[i].content.rendered)
-    );
-  }
+  var main = document.getElementsByTagName('main')[0];
+  var loading = document.getElementById('loading');
+  if (loading) loading.parentElement.removeChild(loading);
+
+  arr.map(function (data) {
+    return makeArticle(data.title.rendered, data.content.rendered);
+  }).forEach(main.appendChild.bind(main));
 }
 
 getArticles(category, getRenderFn(category));
