458bcd41d131e2241b410e8f39aafec1fb7aca75
Steven Frieson <sfrieson@gmail.com>
2024-07-06 11:26:34 -0400

    content updates

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -52,13 +52,6 @@ function apiRequest(pathname, callback) {
   var query = "";
 
   switch (pathname) {
-    case "/about.html":
-      endpoint = "/entries/2JxwNt394sAwY6I4GqIquQ";
-      break;
-    case "/current.html":
-      endpoint = "/entries";
-      query = "?content_type=work";
-      break;
     case "/choreography.html":
       endpoint = "/entries";
       query = "?content_type=choreography";
@@ -66,6 +59,9 @@ function apiRequest(pathname, callback) {
     case "/writing.html":
       endpoint = "/entries";
       query = "?content_type=publication";
+      break;
+    default:
+      return;
   }
 
   xhr(base + "/spaces/" + space + endpoint + query, key, function (response) {
@@ -73,15 +69,6 @@ function apiRequest(pathname, callback) {
       callback(response);
     });
   });
-}
-
-// about.html
-function renderAbout(data) {
-  var div = createElement("div");
-
-  div.appendChild(createElement("div", null, markdownToHtml(data.fields.body)));
-
-  return div;
 }
 
 // choreography.html
@@ -132,83 +119,6 @@ function renderChoreography(data) {
   return ul;
 }
 
-// content.html
-function renderCurrent(data) {
-  var ul = createElement("ul", { class: "current-works" });
-
-  data.items
-    .sort(function (a, b) {
-      var aDate = a.fields.ate;
-      var bDate = b.fields.ate;
-      if (aDate > bDate) return -1;
-      else if (aDate < bDate) return 1;
-      else return 0;
-    })
-    .map(function (item) {
-      var fields = item.fields;
-
-      var section = createElement("section", {
-        itemscope: "",
-        itemtype: "http://schema.org/CreativeWork",
-        class: "current-works__item",
-      });
-
-      if (fields.image) {
-        var img = findAsset(data.includes.Asset, fields.image.sys.id);
-
-        var imgContainer = createElement("div", {
-          class: "list-item__image-container",
-        });
-        imgContainer.appendChild(
-          createElement("img", {
-            class: "list-item__image",
-            src: img.fields.file.url,
-            itemprop: "image",
-            alt: img.fields.title,
-          })
-        );
-        section.appendChild(imgContainer);
-      }
-
-      var listItemInfo = createElement("div", { class: "list-item__info" });
-
-      // TODO Restore when a current work has a title again
-      // listItemInfo.appendChild(
-      //   createElement('h3', {class: 'list-item__title', itemprop: 'name'}, fields.title)
-      // );
-
-      listItemInfo.appendChild(
-        createElement("p", { itemprop: "about" }, fields.description)
-      );
-      if (fields.location) {
-        listItemInfo.appendChild(
-          createElement("p", { itemprop: "locationCreated" }, fields.location)
-        );
-      }
-      if (fields.date) {
-        listItemInfo.appendChild(
-          createElement(
-            "time",
-            { datetime: fields.date, itemprop: "datePublished" },
-            dateToText(fields.date)
-          )
-        );
-      }
-
-      section.appendChild(listItemInfo);
-
-      var li = createElement("li");
-      li.append(section);
-
-      return li;
-    })
-    .forEach(function (li) {
-      ul.appendChild(li);
-    });
-
-  return ul;
-}
-
 function findAsset(assets, id) {
   var found = null;
   for (var i = 0; i < assets.length; i++) {
@@ -241,7 +151,7 @@ function renderPublications(data) {
         itemtype: "http://schema.org/CreativeWork",
       });
       section.appendChild(
-        createElement("h2", { itemprop: "name" }, fields.title)
+        createElement("h4", { itemprop: "name" }, fields.title)
       );
       // sortDate
       // attachment?
@@ -312,14 +222,8 @@ function renderContent(res) {
 
   var content;
   switch (window.location.pathname) {
-    case "/about.html":
-      content = renderAbout(res);
-      break;
     case "/choreography.html":
       content = renderChoreography(res);
-      break;
-    case "/current.html":
-      content = renderCurrent(res);
       break;
     case "/writing.html":
       content = renderPublications(res);
